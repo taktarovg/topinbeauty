fb6cee1cb866a37d56964f4cbdc7da723a0afa9b
taktarovgv <taktarovgv@gmail.com>
2025-02-23 23:08:48 +0700

    use client 43

--- src/providers/ServerAuthProvider.tsx
+++ src/providers/ServerAuthProvider.tsx
@@ -1,9 +1,9 @@
 // src/providers/ServerAuthProvider.tsx
-
 import { cookies } from 'next/headers';
 import { getSession } from '@/lib/session';
+import { ReactNode } from 'react'; // Импортируем ReactNode
 
-export default async function ServerAuthProvider({ children }) {
+export default async function ServerAuthProvider({ children }: { children: ReactNode }) {
   const session = await getSession({ cookies: cookies() });
 
   return (
